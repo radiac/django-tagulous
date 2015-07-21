aa44b64e3d343e81891b639278ac5781bc039134
Richard Terry <git@radiac.net>
2015-07-21 15:07:12 +0100

    Make BaseTagTreeModel available on tagulous.models

--- tagulous/models/__init__.py
+++ tagulous/models/__init__.py
@@ -4,7 +4,9 @@ Tagulous models
 Make important functions and classes available on tagulous.models
 """
 from tagulous.models.options import TagOptions
-from tagulous.models.models import BaseTagModel, TagModel, TagTreeModel
+from tagulous.models.models import (
+    BaseTagModel, TagModel, BaseTagTreeModel, TagTreeModel,
+)
 from tagulous.models.managers import (
     SingleTagManager, TagRelatedManagerMixin,
 )
