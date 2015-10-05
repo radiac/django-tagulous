92bcb33fb2308fe276386f555a1eb4a7472fefab
Richard Terry <git@radiac.net>
2015-10-05 13:25:26 +0100

    Fix conf in setup.py

--- setup.py
+++ setup.py
@@ -112,6 +112,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
