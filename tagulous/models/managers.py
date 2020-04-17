a789370ffa23e4c82297ccc743fa8871af912a37
phamk <20815568@student.uwa.edu.au>
2020-04-17 11:13:51 +0800

    fixes #42 (tag prefetches don't provide any benefit)

--- tagulous/models/managers.py
+++ tagulous/models/managers.py
@@ -241,6 +241,20 @@ class BaseTagRelatedManager(object):
 
     Provides methods to managed cached tags
     """
+    @property
+    def tags(self):
+        if not hasattr(self, '_tags') or self._tags is None:
+            try:
+                self._tags = list(self.all())
+            except AttributeError:
+                self._tags = []
+
+        return self._tags
+
+    @tags.setter
+    def tags(self, value):
+        self._tags = value
+
     def init_tagulous(self, descriptor):
         """
         Called directly after the mixin is added to the instantiated manager
@@ -252,7 +266,6 @@ class BaseTagRelatedManager(object):
         # If internal tags are None, haven't been loaded yet
         self.changed = False
         self.tags = None
-        self.reload()
 
     def __str__(self):
         """
