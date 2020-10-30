e70111fed4bf3123ebd0da3e342fdb74ee930ffa
Dany <56896461+dany-nonstop@users.noreply.github.com>
2020-10-30 10:48:30 -0700

    Update fields.py

--- tagulous/models/fields.py
+++ tagulous/models/fields.py
@@ -11,7 +11,7 @@ from __future__ import unicode_literals
 
 import django
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 from django.utils.text import capfirst
 
 from tagulous import constants, forms
