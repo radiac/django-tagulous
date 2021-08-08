fa2aecb54b44311fe318115eb89b89de0eb206cf
Richard Terry <git@radiac.net>
2021-08-08 11:21:07 +0100

    Update supported versions in readme and setup

--- setup.py
+++ setup.py
@@ -169,6 +169,7 @@ setup(
         "Framework :: Django :: 2.2",
         "Framework :: Django :: 3.0",
         "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
     ],
     install_requires=["Django>=2.2"],
     extras_require={"dev": ["tox", "jasmine"], "devdb": ["psycopg2", "mysqlclient"],},
